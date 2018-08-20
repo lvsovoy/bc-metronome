7f19d6805504c0f7af4026bef3dfbbc84a0997b4
Konstantins Lesovojs <k.lesovoj@gmail.com>
2018-08-20 16:54:17 +0300

    added fonts and icon
    main class still broken, fixing dependencies

--- app/src/main/java/me/lesovoy/metronome/MainActivity.kt
+++ app/src/main/java/me/lesovoy/metronome/MainActivity.kt
@@ -1,24 +1,26 @@
 package me.lesovoy.metronome
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity
+//import android.support.design.widget.Snackbar
+//import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 
 import kotlinx.android.synthetic.main.activity_main.*
+import me.lesovoy.metronome.R.styleable.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+//        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+//        fab.setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                    .setAction("Action", null).show()
+//        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
